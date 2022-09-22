549167245f2c1cb2c038356ce100caa9532e3fd8
AlaminDolar <amin15-9898@diu.edu.bd>
2022-09-22 23:50:06 +0600

    banner added

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import chair from '../../assets/images/chair.png';
+
+const Banner = () => {
+    return (
+        <div className="min-h-screen px-12 hero bg-[url('/src/assets/images/bg.png')] ">
+            <div className="flex-col hero-content lg:flex-row-reverse">
+                <img src={chair} className="max-w-sm rounded-lg shadow-2xl" />
+                <div>
+                    <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+                    <p className="py-6 ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate, veritatis. Quis repellat omnis vitae saepe? Molestias atque, officia laborum repudiandae dolor sit quaerat, reiciendis explicabo non dolorem, mollitia sint blanditiis!</p>
+                    <button className="font-bold text-white btn-primary btn bg-gradient-to-r from-primary-500 to-secondary-500">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
